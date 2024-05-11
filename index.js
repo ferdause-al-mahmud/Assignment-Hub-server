9c6705110f9dd0e4ac7ad1522b7b634495357a53
mahmud444t <ferdausemahmud@gmail.com>
2024-05-11 18:13:16 +0600

    get specific id info

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.port || 4444;
-
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dnxxphb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,6 +31,13 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+        app.get('/assignments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await collection.findOne(query);
+            res.send(result);
+        })
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
