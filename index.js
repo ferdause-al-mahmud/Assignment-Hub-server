b04e5c171f6e7e1b4265063cd792a6c23337a2f9
mahmud444t <ferdausemahmud@gmail.com>
2024-05-14 12:13:07 +0600

    jwt token secured

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const app = express()
 const port = process.env.port || 4444;
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({
+    origin:
+        [
+            'http://localhost:5173',
+            'https://assignment-hub-e4e39.web.app',
+            'https://assignment-hub-e4e39.firebaseapp.com'
+        ],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dnxxphb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -18,6 +30,29 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+
+const logger = (req, res, next) => {
+    console.log('log:info ', req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    console.log("token in the middleware", token)
+    if (!token) {
+        return res.status(401).send({ message: 'ja beta Unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+    // next(.);
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -27,6 +62,31 @@ async function run() {
         const collection = client.db("assignmentHub").collection("assignments");
         const createdAssignments = client.db("assignmentHub").collection("created");
         const submittedCollections = client.db("assignmentHub").collection("submittedAssignments");
+
+        //jwt api
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log("user token", user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            })
+                .send({ success: true })
+        })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none',
+                maxAge: 0  // Setting maxAge to 0 to expire the cookie immediately
+            }).send({ success: true });
+        });
 
         app.get('/assignments', async (req, res) => {
             const cursor = collection.find();
@@ -43,7 +103,7 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
-        app.get('/allAssignments/:id', async (req, res) => {
+        app.get('/allAssignments/:id', verifyToken, logger, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await createdAssignments.findOne(query);
@@ -99,14 +159,22 @@ async function run() {
         })
 
 
-        app.get('/attemptedAssignments/:email', async (req, res) => {
+        app.get('/attemptedAssignments/:email', verifyToken, logger, async (req, res) => {
+
+            console.log(req.params.email)
+            console.log("token owner info", req.user.email)
+            if (req.user.email !== req.params.email) {
+
+                return res.status(403).send({ message: 'forbidden access' })
+
+            }
             const submitter = req.params.email;
             const query = { submitter_email: submitter };
             const cursor = submittedCollections.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
-        app.get('/pendingAssignments', async (req, res) => {
+        app.get('/pendingAssignments', verifyToken, logger, async (req, res) => {
             const { status } = req.query;
             let query = {};
             if (status) {
@@ -116,7 +184,7 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
-        app.get('/pendingAssignments/:id', async (req, res) => {
+        app.get('/pendingAssignments/:id', verifyToken, logger, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await submittedCollections.findOne(query);
