cafa75687d70c364c8c297415f9d7f497ceb284a
mahmud444t <ferdausemahmud@gmail.com>
2024-05-15 18:12:53 +0600

    bug fixxed

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
-        app.get('/allAssignments/:id', verifyToken, logger, async (req, res) => {
+        app.get('/allAssignments/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await createdAssignments.findOne(query);
